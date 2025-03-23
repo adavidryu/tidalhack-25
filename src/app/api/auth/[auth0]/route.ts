0ef5b766d90b6336087bf50824b73c7f0bcd8dfa
adavidryu <adavidryu@gmail.com>
2025-03-23 12:20:50 -0500

    change auth0 route

--- src/app/api/auth/[auth0]/route.ts
+++
@@ -1,9 +0,0 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
-
-export async function GET(
-  request: Request,
-  { params }: { params: { auth0: string } }
-) {
-  const handler = handleAuth();
-  return handler(request, { params });
-} 
